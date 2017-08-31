57a015bc05ebbd94a535075ce3334e6c9e5b0f80
carlosedugoc <carlosedugoc@gmail.com>
2017-08-30 20:21:07 -0500

    Se agregan cabeceras http al app.js

--- ApiResTvfullstack/app.js
+++ ApiResTvfullstack/app.js
@@ -12,7 +12,15 @@ var recurso = require('./routes/recurso');
 app.use(body.urlencoded({ extended: false }));
 app.use(body.json());
 
-//Configurar cabe
+//Configurar cabeceras http
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-Width, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+
 
 //rutas base
 app.use('/api', recurso);
